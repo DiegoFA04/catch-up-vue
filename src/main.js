1ce0a2a03abe850d70b91041e91cc01f83237353
Diego Flores Avalos <129472574+DiegoFA04@users.noreply.github.com>
2024-04-18 23:59:23 -0500

    feat(i18n): configured i18n support.

--- src/main.js
+++ src/main.js
@@ -22,6 +22,7 @@ import 'primeicons/primeicons.css'
 
 // Add App Theme
 import 'primevue/resources/themes/md-light-indigo/theme.css'
+import i18n from "./i18n.js";
 
 
 const app = createApp(App);
@@ -36,6 +37,9 @@ app.use(PrimeVue, { ripple: true })
     .component('pv-menu', Menu)
     .component('pv-menubar', Menubar)
     .component('pv-toolbar', Toolbar);
+
+// Add i18n Plugin
+app.use(i18n);
 
 //Mount in app div
 app.mount('#app');
